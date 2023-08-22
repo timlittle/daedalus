f5375c27e64f1b5c6a533ee7d6740606819cfec8
Tim Little <timlittle88@gmail.com>
2023-08-22 05:07:58 +0100

    Fix build by stubbing onAction call

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default async function Home() {
                     description={project.description}
                     currentUser={currentUser}
                     key={project.id}
+                    onAction={()=>{}}
                   />
               );
             })}
